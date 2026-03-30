fb0f11da273b76b1e4c733fb8eb6b878d8263aec
timothyoshea <toshea@gmail.com>
2026-03-30 09:54:32 +0100

    Auto-commit: update scan.py

--- api/scan.py
+++ api/scan.py
@@ -126,8 +126,6 @@ def run_scan_and_save():
         "sure_bets": sure_bets,
         "edge_bets": edge_bets,
         "safe_no_bets": safe_no_bets,
-        "debug_log": debug_log,
-        "config_tomorrow": config.TOMORROW,
     }
 
 
