2527c9c8b2ef877ec08760e78596bbf7daa05a3c
timothyoshea <toshea@gmail.com>
2026-04-11 08:34:16 +0100

    Auto-commit: update app.py

--- railway-trader/app.py
+++ railway-trader/app.py
@@ -987,4 +987,4 @@ def redeem():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
